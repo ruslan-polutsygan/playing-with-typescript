e5262d5b78f8a4a88c6ca337d59edc709aa0d059
ruslan-polutsygan <ruslan.polutsygan@gmail.com>
2015-01-05 09:35:13 +0200

    modules

---
+++ test4.ts
@@ -0,0 +1,29 @@
+module Validation {
+    export interface StringValidator {
+        isAcceptable(s: string): boolean;
+    }
+
+    var minLength = 5;
+    export class MinLengthValidator implements StringValidator {
+        isAcceptable(s: string): boolean {
+            return s.length >= minLength;
+        }
+    }
+
+    var lettersRegExp = /^[a-z]+$/i;
+    export class LettersOnlyValidator implements StringValidator {
+        isAcceptable(s: string): boolean {
+            return lettersRegExp.test(s);
+        }
+    }
+}
+
+
+var validators: { [x: string]: Validation.StringValidator } = {};
+validators['Min Length'] = new Validation.MinLengthValidator();
+validators['Letters Only'] = new Validation.LettersOnlyValidator();
+['ddddd', 'ss', '23sss'].forEach(s => {
+    for(var v in validators) {
+        console.log(s + ' ' + v + ' ' + (validators[v].isAcceptable(s)?'ok':'bad'));
+    }
+});
